03b900bdb5e78c657cd99cb37261cb851c4494aa
Vipin Jain <jan.vipin@gmail.com>
2013-03-22 19:43:29 -0400

    weekend commit

--- redirect.py
+++ redirect.py
@@ -4,7 +4,8 @@ import urllib.request
 import sys
 import json
 from multiprocessing import Queue, Process
-# httplib.HTTPConnection.debuglevel = 1
+from sqlalchemy.orm import Session, sessionmaker
+import sqlalc_utilities as sqa
 
 class RedirectionHandler(urllib.request.HTTPRedirectHandler):
     """ Overrides RedirectionHandler to output 
@@ -22,24 +23,56 @@ def RedirectUrls(inputq, outputq):
     """ Takes the list of Url's as input and writes
     the input record and the number of redirects to a file"""
     
-    # def __init__(self, inputf, outputf):
-    #     self.inputf = open(inputf, 'r', encoding='UTF-8')
-    #     self.outputf = open(outputf, 'w', encoding='UTF-8')
-    #     self.count = 0
+    db_conn = sqa.connect_db()
+    Session = sessionmaker(bind=db_conn) 
+    session = Session()
 
-    # def __del__(self):
-    #     self.inputf.close()
-    #     self.outputf.close()
-
-    # for line in self.inp:
     item = inputq.get()
     rec = json.loads(item)
-    url = rec['twit_url']
-    request = urllib.request.Request(url)
+    tco_url = rec['twit_url']
+    exp_url = rec['expanded_url']
+    print "Original: ", tco_url, exp_url
+
+    try:
+        # exp_url = str.lower(urllib.unquote_plus(exp_url)) 
+        exp_url = str.lower(exp_url)  # Expect the encoding errors here
+    except:
+        print "Terminating: ", exp_url
+        self.terminate()
+    
+    # URL processing 
+    if exp_url.find('http://') == -1:
+        dom = exp_url
+    else:
+        #exp_url =  http://via.me/-9u5qho0" # normal urls
+        dom = dom.split('/')[2]
+        # exp_url = http://biebervideo55.tk?=irgvhrxr, no / at the end, abnormal urls
+        if dom.find('?') != -1:
+            dom = dom.split('?')[0]
+            
+    # Check whether it exists in the database
+    in_domains = session.query(sqa.Domains).filter(sqa.Domains.exp_url == dom).all()
+    in_exp_urls = session.query(sqa.ExpUrls).filter(sqa.ExpUrls.exp_url == exp_url).all()
+
+    if not in_domains:
+        domain = sqa.Domains(domain_name=dom)
+        session.add(domain)
+        print "Added in Domains"
+        
+    if not in_exp_urls:
+        url_rec = sql.ExpUrls(exp_url=exp_url,tco_url=url)
+        session.add(url_rec)
+        print "Added in exp_urls"
+    else:
+        db_record = session.query(sqa.UrlRecs).filter(sqa.UrlRecs.exp_url = exp_url).all()
+        if db_record.num_redir > 0:
+            self.terminate()
+    
+    request = urllib.request.Request(exp_url)
     redirect_list = []
     opener = urllib.request.build_opener(RedirectionHandler(redirect_list))
     try:
-        s = opener.open(url)
+        s = opener.open(exp_url)
     except:
         pass
     finally:
